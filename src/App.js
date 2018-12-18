7e6fa67eddbf8f9b3a660ed0bfae1ee59f5b54b3
Kate <katerina_ti@mail.ru>
2018-12-18 20:07:09 +0300

    working data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      games: []
+    }
+}
+
+
   render() {
+    const key = "2D7A611545098E1FC165D53E16F03065";
+    const id = "76561198080321262";
+    axios.get(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${key}&steamid=${id}&format=json`)
+      .then(response =>  response.data.response.games ? this.setState({games: response.data.response.games}) : null );
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     <div>
+        <table>
+          <thead>
+          <tr>
+            <th>Game's name</th>
+            <th>Game play duration</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            {this.state.games.map((el) => <tr><td>{el.appid}</td><td>{Math.round(el.playtime_forever /60) }</td></tr> )}
+          </tr>
+          </tbody>
+        </table>
+     </div>
+
     );
   }
 }
