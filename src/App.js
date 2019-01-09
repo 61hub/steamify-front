b7b6f4b7e88845e50996c9892ebee340c59d9e9f
Kate <katerina_ti@mail.ru>
2019-01-09 13:43:42 +0300

    fixed styles

--- src/App.js
+++ src/App.js
@@ -7,24 +7,31 @@ class App extends Component {
     super(props);
     this.state = {
       games: []
-    }
+    };
+    this.handleScroll = this.handleScroll.bind(this);
 }
 
+  handleScroll = () => {
+    window.scrollTo({
+      top: 1000,
+      behavior: "smooth"
+    })
+  };
 
   render() {
-    // const key = "2D7A611545098E1FC165D53E16F03065";
-    // const id = "76561198080321262";
+
     axios.get(`http://157.230.56.14:3000/GetOwnedGames`)
       .then(response => response.data.games ? this.setState({games: response.data.games}) : null);
 
     const mappedId = this.state.games.map((el) => el.appid);
     const joinedId = mappedId.join();
 
-
     // axios.get(`https://store.steampowered.com/api/appdetails/?appids=${joinedId}&cc=us&filters=price_overview`)
     //   .then(response => console.log(response) );
+
     return (
-     <div className="container">
+     <div className="container" onClick={this.handleScroll}>
+       <div className="overlay">
         <table>
           <thead>
           <tr>
@@ -40,10 +47,11 @@ class App extends Component {
                  <img src={"http://media.steampowered.com/steamcommunity/public/images/apps/"
                + el.appid + "/" + el.img_icon_url + ".jpg"}></img></td>
                 <td>{el.name}</td>
-                <td>{Math.round(el.playtime_forever / 60)}</td>
+                <td className="gameDuration">{Math.round(el.playtime_forever / 60)}</td>
               </tr> )}
           </tbody>
         </table>
+       </div>
      </div>
 
     );
