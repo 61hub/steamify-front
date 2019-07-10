c4c61e53a5374cb3c5879ec09150499a4d9b5526
Victor <sexsportandrocknroll@gmail.com>
2019-07-11 02:18:55 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2205ac698e47e8a6cf5173a223be01b6.js"
+  "/precache-manifest.3c930c86ec666a2ff91a3dec118bb47f.js"
 );
 
 workbox.clientsClaim();
