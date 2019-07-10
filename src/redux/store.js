34b53a7f8c20b93160217f24e40982d5ec37de5e
Victor <sexsportandrocknroll@gmail.com>
2019-07-11 01:14:04 +0300

    mobile redux-devtools fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import {createStore, compose, applyMiddleware} from "redux"
 import thunk from "redux-thunk";
 import { gamesReducer } from "./reducers/games";
 
+const middlewares = window.__REDUX_DEVTOOLS_EXTENSION__ ? compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__()
+) : applyMiddleware(thunk);
+
 const store = createStore(
   gamesReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  middlewares
 );
 
 export default store
