f1d63951e5002a3a161d67996dca8561e13a4825
Victor <sexsportandrocknroll@gmail.com>
2019-07-19 02:38:07 +0300

    Updates

---
+++ precache-manifest.35e5a52ae1c164484c7b6557b27eb4b0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a82b41082b04f774d82317dff6874d14",
+    "url": "/static/media/icons-16.a82b4108.ttf"
+  },
+  {
+    "revision": "f3ffeee5447836c0ae60",
+    "url": "/static/css/main.a0b6f733.chunk.css"
+  },
+  {
+    "revision": "5502b03b7b07c388f20f77fa469a1f47",
+    "url": "/static/media/icons-20.5502b03b.ttf"
+  },
+  {
+    "revision": "a293b9d572cecc1c04d2",
+    "url": "/static/js/1.a293b9d5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7f037192fcdef96884010177e90e8c5f",
+    "url": "/static/media/icons-16.7f037192.eot"
+  },
+  {
+    "revision": "f3ffeee5447836c0ae60",
+    "url": "/static/js/main.f3ffeee5.chunk.js"
+  },
+  {
+    "revision": "f1bd87971a332ecb21b4ac1d19537fa3",
+    "url": "/static/media/icons-20.f1bd8797.eot"
+  },
+  {
+    "revision": "422aba8639312ac889d34200226a5c4e",
+    "url": "/static/media/icons-16.422aba86.woff"
+  },
+  {
+    "revision": "52f2f35bf3a635ca696e95cc33cf12fa",
+    "url": "/static/media/icons-20.52f2f35b.woff"
+  },
+  {
+    "revision": "a293b9d572cecc1c04d2",
+    "url": "/static/css/1.67124362.chunk.css"
+  },
+  {
+    "revision": "07c183a2bd0f69caaed07923f4c02dfc",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2004a52d634419db230c0b4d2a946b72.js"
+  "/precache-manifest.35e5a52ae1c164484c7b6557b27eb4b0.js"
 );
 
 workbox.clientsClaim();
